0a4e3c66b41bdf73736602b6c56d559f1ea26c02
Rudi MK <rudimk.cloud@gmail.com>
2021-06-14 15:52:31 +0000

    Added a missing import for java.util.List.

--- plugins/hackerbook/feature/src/main/java/org/apache/cloudstack/feature/CoffeeManager.java
+++ plugins/hackerbook/feature/src/main/java/org/apache/cloudstack/feature/CoffeeManager.java
@@ -4,6 +4,7 @@ import org.apache.cloudstack.api.command.CreateCoffeeCmd;
 import org.apache.cloudstack.api.command.ListCoffeesCmd;
 
 import java.util.Hashtable;
+import java.util.List;
 
 public interface CoffeeManager {
     Hashtable<String, String> createCoffee(CreateCoffeeCmd cmd);
